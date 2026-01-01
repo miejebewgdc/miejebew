7c6f9a475672d9f4beacaeaf9f58e7b6d1c604da
unknown <taufiqrusdhi.ez@gmail.com>
2026-01-01 12:48:45 +0700

    menyelesaikan merge dan update link gambar

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    base: '/miejebew/', // Nama repo harus diapit garis miring
+    base: './', // Menggunakan './' agar path menjadi relatif
     plugins: [react()],
     resolve: {
         alias: {
